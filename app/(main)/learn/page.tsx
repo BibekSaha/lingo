7d0ba38db24a3052da31a183d2d57d71d6fe91a7
Bibek Saha <thebibeksaha@gmail.com>
2024-07-21 01:09:51 +0530

    Added Learn Page Wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,22 @@
+import { FeedWrapper } from '@/components/feedWrapper';
+import { StickyWrapper } from '@/components/stickyWrapper';
+import { Header } from './header';
+import { UserProgress } from '@/components/userProgress';
+
 export default function LearnPage() {
   return (
-    <div>Learn Page</div>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+    </div>
   );
 }
